a63dc9f71485527f01dc64c38bd7db7d115718ef
Davi Oliveira <davicicfaculdade@gmail.com>
2023-10-10 16:16:46 -0300

    Scraper do Artigo quase completo.

--- app/lib/scielo/article.ts
+++ app/lib/scielo/article.ts
@@ -1,50 +1,178 @@
-import { Element, load } from "cheerio";
+import { load, Element, Cheerio } from "cheerio";
 
 type Type = "legacy" | "br" | "preprint";
 
-interface ArticleData {
-  type: Type,
-  title?: string,
+interface Content {
+  type: "text" | "image" | "table",
+  value: string | null | undefined
 }
 
-async function scrapeDefaultArticle(html: string): Promise<any> {
-  const $ = load(html);
-  return {
-    type: "br",
-    title: $('.article-title').text(),
+interface Text extends Content {
+  quote: boolean,
+}
+
+interface Image extends Content {
+  id: string | undefined,
+}
+
+interface Table extends Image {
+  isImage: boolean,
+  label: {
+    name: string,
+    description: string,
   }
 }
 
-async function scrapeLegacyArticle(html: string): Promise<any> {
+interface Section {
+  title: string,
+  isSubtitle: boolean,
+  content: Array<Text | Image | Table>,
+}
+
+interface Abstract {
+  title: string | undefined,
+  text: string | undefined,
+  keywords: {
+    label: string | undefined,
+    text: string | undefined
+  },
+}
+
+interface Authors {
+  name: string,
+  id: string,
+}
+
+interface AuthorDescription {
+  [key: string]: string,
+}
+
+function getSectionData(section: Element, host: string) {
+  const $ = load(section);
+
+  const thisSection: Section = {
+    title: $('.sec, .sub-subsec').text(),
+    isSubtitle: $('.sub-subsec').length != 0,
+    content: new Array()
+  };
+
+  // Qualquer <p> dentro de um <div>
+  // Qualquer div com a classe .table-wrap ou .figure
+  // Qualquer <blockquote>
+  $('div > p, .table-wrap, blockquote, .figure').each(function () {
+    // Se o elemento não tiver uma classe ele pode ser tanto um blockquote quanto um p
+    // Porém, o blockquote tem um p dentro dele, então eu verifico isso.
+    const type = $(this).attr('class');
+
+    switch (type) {
+      case undefined:
+        thisSection.content.push({
+          type: "text",
+          quote: $(this).has('p').length ? true : false,
+          value: $(this).text()
+        })
+        break;
+      case 'figure':
+        thisSection.content.push({
+          type: "image",
+          id: $(this).find('a').first().attr('name'),
+          value: host + $(this).find('img').attr('src')
+        })
+        break;
+      case 'table-wrap':
+        let table = $(this).find('.table').html();
+        const isImage = !table;
+
+        if (!table) table = host + $(this).find('img').attr('src');
+
+        thisSection.content.push({
+          type: "table",
+          id: $(this).find('a').first().attr('name'),
+          value: table,
+          isImage: isImage,
+          label: {
+            name: $(this).find('.label').text(),
+            description: $(this).find('.caption').text()
+          }
+        })
+        break;
+    }
+  })
+
+  return thisSection;
+}
+
+function getAbstractData(abstract: Element): Abstract {
+  const $ = load(abstract);
+
+  let data: any = {};
+
+  $('p').each(function (i) {
+    if (i == 0) data.title = $(this).text();
+    else if (i == 1) data.text = $(this).text();
+    else {
+      data.keywords = {
+        label: $(this).find('b').text(),
+        // Regex para remover o texto entre <b> </b> que pertence a label.
+        text: ($(this).html() ?? '').replace(/<b>(.*)<\/b>/, ''),
+      }
+    }
+  });
+
+  return data;
+}
+
+function getAuthorsData(a: Cheerio<Element>, d: Cheerio<Element>) {
+  const authors: Authors[] = [];
+  const descriptions: AuthorDescription = {};
+
+  d.each((_, element: any) => {
+    const $ = load(element);
+    const id = '#' + $('a').attr('name') as string;
+    descriptions[id] = element.lastChild?.data ?? '';
+  });
+
+  a.each((_, element) => {
+    const $ = load(element);
+    const id = $('sup > a').attr('href') as string;
+    authors.push({
+      name: $('.author-name').text(),
+      id: id,
+    });
+  });
+
+  return {authors, descriptions};
+}
+
+function getPageHost(url: string) {
+  return url.split('/')[2];
+}
+
+async function scrapeLegacyArticle(html: string, host: string): Promise<any> {
   const $ = load(html);
 
   // Resumo em diversas línguas do artigo
-  const abstracts: any = [];
+  const abstracts: Abstract[] = [];
+  // Todas as seções do texto
+  const sections: Section[] = [];
+  // Autores do artigo, junto com suas descrições;
+  const authors = getAuthorsData($('.author'), $('.aff'));
 
-  $('.trans-abstract').each(function () {
-    const title = $(this).find('.sec').text();
-    let content;
-    let keywords;
+  $('.abstract, .trans-abstract').each((_, abstractElement) => {
+    abstracts.push(getAbstractData(abstractElement));
+  });
 
-    $(this).find('p').filter(function (i) { return $(this).attr('class') != 'sec' }).each(
-      function (i) {
-        if (i == 0) content = $(this).html()
-        else keywords = $(this).html()
-      }
-    )
-
-    abstracts.push({
-      title: title,
-      content: content,
-      keywords: keywords,
-    })
-  })
+  $('.section').each((_, sectionElement) => {
+    sections.push(getSectionData(sectionElement, host));
+  });
 
   return {
     type: "legacy",
     title: $('.title').text(),
     altTitle: getTranslatedTitles($('.trans-title').toArray()),
+    authors: authors,
     abstracts: abstracts,
+    sections: sections
   }
 }
 
@@ -70,12 +198,13 @@ function getTypeFromUrl(url: string): Type {
 export default async function scrapeArticle(url: string): Promise<any> {
   const type = getTypeFromUrl(url);
   const html = await (await fetch(url)).text();
+  const host = getPageHost(url);
 
   switch (type) {
     case 'br':
-      return await scrapeDefaultArticle(html);
+      return 'br';
     case 'legacy':
-      return await scrapeLegacyArticle(html);
+      return await scrapeLegacyArticle(html, host);
     case 'preprint':
       return await scrapePreprintArticle(html);
   }
--- app/scielo/leitor/page.tsx
+++ app/scielo/leitor/page.tsx
@@ -1,17 +1,29 @@
 import scrapeArticle from "@/app/lib/scielo/article";
-import parse from 'html-react-parser'
+import parse from "html-react-parser";
 
-export default async function Leitor({ searchParams }: {
+export default async function Leitor({
+  searchParams,
+}: {
   searchParams: {
-    url: string,
-  }
+    url: string;
+  };
 }) {
   const data = await scrapeArticle(searchParams.url);
+  console.log(data)
   return (
-    <p>
-      <h1>{data.abstracts[1].title}</h1>
-      <p>{parse(data.abstracts[1].content)}</p>
-      <p>{parse(data.abstracts[1].keywords)}</p>
-    </p>
+    /*<div>
+      {data.sections.map((section: any, index: number) => {
+        return <section key={index}>
+          <h1 key={index}>{section.title.value}</h1>
+          {
+            section.content.map((c: any, i: number) => {
+              return <p key={i}>{parse(c.value)}</p>
+            })
+           }
+        </section>
+      })}
+    </div>
+    */
+   <h1>haha</h1>
   );
 }
